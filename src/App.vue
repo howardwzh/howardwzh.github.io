cdb2cc02b053243241a0d8e859574ca4b252c2bf
future <future@gettr.best>
2024-09-07 20:59:09 +0900

    init: new website

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+  justify-content: space-between;
+}
+
+.logo {
+  height: 70px;
+  width: auto;
+}
+
+nav {
+  display: flex;
+  gap: 1rem;
+}
+
+main {
+  flex: 1;
+  padding: 1rem;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+
+const store = useCounterStore()
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>{{ store.count }}</p>
+    <button @click="store.increment()">Increment</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
