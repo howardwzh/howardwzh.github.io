67d1ca748f5f55466c36a1f94f232f6e52b436b1
future <future@gettr.best>
2024-10-03 01:48:53 +0900

    storybook: init vue3(1003-1)

---
+++ storybooks/vue3/assets/index-BYoZv835.js
@@ -0,0 +1,1 @@
+import{M as a,u as n}from"./index-CnyEqRz-.js";import"./index-BQFYldH0.js";import"./iframe-BrfO3taR.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-CxISiSMm.js";import"./index-DrFu-skq.js";export{a as MDXProvider,n as useMDXComponents};
